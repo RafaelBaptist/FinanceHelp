28c17133af565bfe4cdc8e918e43a7afa1464956
Rafael de Souza Baptista <faelbaptista0@gmail.com>
2025-03-25 10:14:15 -0300

    signOut

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Button} from 'react-native';
+import {AuthContext} from '../../contexts/auth';
 
 export default function Home() {
+  const {signOut, user} = useContext(AuthContext);
   return (
     <View>
       <Text>Tela home</Text>
+      <Text>Nome:{user.name}</Text>
+      <Button title="Sair  da  conta" onPress={() => signOut()} />
     </View>
   );
 }
